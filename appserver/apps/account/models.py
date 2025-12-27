5eaa8e984c1ec8b2a6b672d979be4ed2abf7e81d
Dohyun Kim <fanlino@gmail.com>
2025-12-27 23:09:06 +0900

    134p modeling 완료

--- appserver/apps/account/models.py
+++ appserver/apps/account/models.py
@@ -7,7 +7,7 @@ from sqlalchemy import UniqueConstraint
 from sqlalchemy_utc import UtcDateTime
 
 if TYPE_CHECKING:
-    from appserver.apps.calendar.models import Calendar
+    from appserver.apps.calendar.models import Calendar, Booking
 
 
 class User(SQLModel, table=True):
@@ -26,6 +26,8 @@ class User(SQLModel, table=True):
     oauth_accounts: list["OAuthAccount"] = Relationship(back_populates="user")
     calendar: Calendar = Relationship(back_populates="host",
                                       sa_relationship_kwargs={"uselist": False, "single_parent": True})
+
+    bookings: list["Booking"] = Relationship(back_populates="guest")
 
     created_at: AwareDatetime = Field(
         default=None,
